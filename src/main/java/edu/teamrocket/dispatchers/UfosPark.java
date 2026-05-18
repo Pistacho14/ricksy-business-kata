77e1e91ba851117dbf46ab91c6acc1f0a190f9a5
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-05-18 12:34:16 +0200

    style(dispatchers): :art: format code

--- src/main/java/edu/teamrocket/dispatchers/UfosPark.java
+++ src/main/java/edu/teamrocket/dispatchers/UfosPark.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import edu.teamrocket.payment.PaymentMethod;
 import edu.teamrocket.receptivo.GuestDispatcher;
 
-public class UfosPark implements GuestDispatcher{
+public class UfosPark implements GuestDispatcher {
 
     private Map<String, String> flota = new HashMap<>();
     private static final double FEE = 500;
@@ -18,6 +18,7 @@ public class UfosPark implements GuestDispatcher{
     public void add(String ovni) {
         flota.putIfAbsent(ovni, null);
     }
+
     @Override
     public void dispatch(PaymentMethod usuario) {
         if (!flota.containsValue(usuario.number())) {
